515eac0e8bd3eaa2c0049c403fd0c78379b9ade4
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-09-07 23:23:20 -0500

    Update H_Index.java

--- src/H_Index.java
+++ src/H_Index.java
@@ -54,3 +54,30 @@ class H_Index_Binary_Search {
 		return citations.length - low;	// if they don't become equal and citations crosses it, the h index is found at low index
 	}
 }
+
+/********************************************Using Bucket Sort*************************************/
+//Time Complexity :O(n) ,  number of elements in the array
+//Space Complexity :O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+class H_Index_Bucket_sort {
+	public int hIndex(int[] citations) {
+		if(citations == null || citations.length == 0)
+			return 0;
+		int n = citations.length;
+		//bucket sort
+		int[] buckets = new int[n+1];    // one more bucket to store values greater than n in citations array
+		for(int i=0; i<n; i++){
+			buckets[Math.min(citations[i], n)]++;   // increase the value bucket index = citations[i] , if //citations[i] > n then we incraese at last index of buckets array 
+		}
+
+		int sum=0;
+		for(int i=buckets.length-1; i>=0; i--){ //iterate over the buckets array backwards
+			sum+= buckets[i];   //finding the sum of bucket array( number of papers)
+			if(sum >= i)    //when the sum crosses the index(citations)
+				return i;   //return citations
+		}
+		return 0;
+	}
+}
